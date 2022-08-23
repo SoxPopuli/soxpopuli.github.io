8bcee92ba2fb0d03c0ccb00285786fca0eaa2489
Charlotte Smith <charliesmith5019@gmail.com>
2022-08-23 20:27:29 +0100

    basic skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import { Component } from 'react';
+
+import { 
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    NavLink,
+} from 'react-router-dom';
+
+class MenuEntry {
+    name: string
+    to: string
+
+    constructor(name: string, to: string) {
+        this.name = name
+        this.to = to
+    }
+}
+
+class Header extends Component {
+    constructor(props: any) {
+        super(props)
+    }
+
+    readonly menu_items = [
+        new MenuEntry("Home", "/"),
+        new MenuEntry("Test A", "/test1"),
+        new MenuEntry("Test B", "/test2"),
+    ]
+
+    render() {
+        let menu = this.menu_items.map(e => 
+            <li key={e.name}>
+                <NavLink to={e.to}>{e.name}</NavLink>
+           </li>
+        );
+
+        return (
+            <div className="menu-container">
+                <h1>Hello</h1>
+                <ul className='menu-list'>
+                    {menu}
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+class Home extends Component {
+    constructor(props: any) {
+        super(props)
+    }
+
+    render() {
+        return <p>Homepage</p>
+    }
+
+}
+
+function Test1() {
+    return <p>test 1</p>;
+}
+
+function Test2() {
+    return <p>test 2</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router basename={`/${process.env.PUBLIC_URL}`}>
+          <Header/>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/test1" element={<Test1/>}/>
+              <Route path="/test2" element={<Test2/>}/>
+          </Routes>
+      </Router>
   );
 }
 
